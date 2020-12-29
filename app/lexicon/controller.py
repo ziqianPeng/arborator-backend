d4d970795b0118d50bc884f7ba7349adbdf61ce0
LilaKIM <lila.kim@sorbonne-nouvelle.fr>
2020-12-29 23:58:28 +0100

    transformation grew

--- app/lexicon/controller.py
+++ app/lexicon/controller.py
@@ -53,7 +53,7 @@ class LexiconExportJson(Resource):
 
 
 @api.route("/<string:project_name>/export/tsv")
-class LexiconExportJson(Resource):
+class LexiconExportTsv(Resource):
     def post(self, project_name: str):
         parser = reqparse.RequestParser()
         parser.add_argument(name="data", type=dict, action="append")
@@ -84,30 +84,39 @@ class TransformationGrewResource(Resource):
         patterns = []
         commands = []
         without = ""
-        dic = {0: "form", 1: "lemma", 2: "upos", 3: "_MISC_Gloss", 4: "trait"}
-        for i in lexicon:
-            line1 = i["currentInfo"].split(" ")
-            line2 = i["info2Change"].split(" ")
-            comp += 1
+        dic = {
+            0: "form",
+            1 : "lemma",
+            2 : "upos",
+            3 :"Gloss",
+            4 : "trait"
+            }
+        for i in lexicon['data'] :
+            rule_grew = "pattern {"
+            #print(i['info2Change'])
+            line1 = i['currentInfo'].split(' ')
+            line2 = i['info2Change'].split(' ')
+            #print(line2)
+            comp+=1
             patterns.append(transform_grew_get_pattern(line1, dic, comp))
+            rule_grew += patterns[comp-1]+'}'
             resultat = transform_grew_verif(line1, line2)
-            co, without_traits = transform_grew_get_commands(
-                resultat, line1, line2, dic, comp
-            )
+            co, without_traits = (transform_grew_get_commands(resultat,line1, line2, dic, comp))
             commands.append(co)
-            if without_traits != "":
+            if without_traits != '' : 
+                if without != "" :
+                    without += ", "
                 without = without + without_traits
-        patterns[0] = (
-            "% click the button 'Correct lexicon' to update the queries\n\npattern { "
-            + patterns[0][0:]
-        )
-        commands[0] = "commands { " + commands[0][0:]
-        patterns[len(lexicon["data"]) - 1] += " }"
-        commands.append("}")
-        if len(without) != 0:
-            without = "\nwithout { " + without + "}"
-        patterns_output = ",".join(patterns)
-        commands_output = "".join(commands)
+                rule_grew += " without{ "+without_traits+"}"
+            rule_grew += " command{ " + commands[comp-1]+"}"
+        patterns[0] = '% click the button \'Correct lexicon\' to update the queries\n\npattern { ' + patterns[0][0:]
+        commands[0] = 'commands { '+ commands[0][0:]
+        patterns[len(lexicon['data'])-1] += ' }'
+        commands.append('}')
+        if len(without) != 0 :
+            without = '\nwithout { ' + without + '}'
+        patterns_output = ','.join(patterns)
+        commands_output = ''.join(commands)
         resp = {
             "patterns": patterns_output,
             "commands": commands_output,
@@ -118,7 +127,7 @@ class TransformationGrewResource(Resource):
         return resp
 
 
-# TODO : It seems that this function is not finished. Ask Lila what should be done
+# TODO : It seems that this function is not finished. Ask Lila what should be done -> finished function
 @api.route("/<project_name>/upload/validator", methods=["POST", "OPTIONS"])
 class LexiconUploadValidatorResource(Resource):
     def post(self, project_name):
@@ -142,61 +151,65 @@ class LexiconAddValidatorResource(Resource):
         lexicon = args.get("data")
         validator = args.get("validator")
         list_validator = []
-        line = []
+        line=[]
         A = []
         B = []
-        AB_Ok = []
-        AB_Diff = []
+        AB_Ok=[]
+        AB_Diff=[]
         list_types = {
-            "In the two dictionaries with the same information": AB_Ok,
-            "In the two dictionaries with different information": AB_Diff,
-            "Only in the old dictionary": A,
-            "Only in the imported dictionary": B,
-        }
+            "In the two dictionaries with the same information" : AB_Ok,
+            "Identical form in both dictionaries with different information" : AB_Diff,
+            "Only in the old dictionary" : A,
+            "Only in the imported dictionary" : B}
 
-        for i in validator["validator"].split("\n"):
+        for i in validator['validator'].split('\n'):
             a = i.split("\t")
-            if a[-1] == "":
+            if a[-1] == '': 
                 a.pop()
-            if a != []:
+            if a != []: 
                 a[-1] = a[0] + a[1] + a[2] + a[3] + a[4]
                 newjson = {
-                    "form": a[0],
-                    "lemma": a[1],
-                    "POS": a[2],
-                    "features": a[3],
-                    "gloss": a[4],
-                    "key": a[-1],
-                }
+                    "form":a[0],
+                    "lemma":a[1],
+                    "POS":a[2],
+                    "features":a[3],
+                    "gloss":a[4],
+                    "key":a[-1],
+                    }
                 list_validator.append(newjson)
         # print("lexicon = \n", list_lexicon, "\n\nval = \n", list_validator)
 
-        for x in lexicon["data"]:
-            if "frequency" in x:
-                del x["frequency"]
+        
+        for x in lexicon['data']:
+            if 'frequency' in x: 
+                del x['frequency']
             for y in list_validator:
-                if x["key"] == y["key"] and x not in AB_Ok and x not in AB_Diff:
+                # le token existe dans les deux dicts avec les mêmes feats
+                if x['key'] == y['key'] and x not in AB_Ok and x not in AB_Diff: 
+                    x['toChange'] = "_"
                     AB_Ok.append(x)
-                elif (
-                    x["key"] != y["key"]
-                    and x["form"] == y["form"]
-                    and x not in AB_Ok
-                    and x not in AB_Diff
-                    and y not in AB_Ok
-                    and y not in AB_Diff
-                ):
-                    AB_Diff.extend((x, y))
+                # le terme existe dans les deux dictionnaires mais avec de différents feats
+                elif x['key'] != y['key'] and x['form'] == y['form'] and x not in AB_Ok and x not in AB_Diff and y not in AB_Ok and y not in AB_Diff: 
+                    x['toChange'] = y['form'] + ' ' + y['lemma'] + ' ' + y['POS'] + ' ' + y['gloss'] + ' ' + y['features']
+                    AB_Diff.extend((x,y))
 
-        for x in lexicon["data"]:
+        # le token n'existe pas dans le dict A
+        for x in lexicon['data']:
             if x not in AB_Ok and x not in AB_Diff and x not in A:
+                x['toChange'] = "_"
                 A.append(x)
+
+        # le token n'existe pas dans le dict B
         for y in list_validator:
-            if y not in AB_Ok and y not in AB_Diff and x not in B:
+            if y not in AB_Ok and y not in AB_Diff and x not in B: 
+                y['toChange'] = "_"
                 B.append(y)
 
+        # print("AAAAAAA ",A,"\n\nBBBBBBBB ",B, "\n\nAB OK", AB_Ok, "\n\nAB Diff", AB_Diff)
+        
         for i in list_types:
             for s in list_types[i]:
-                s["type"] = i
+                s['type'] = i
                 line.append(s)
         # print(line)
         resp = {"dics": line, "message": "hello"}
@@ -211,54 +224,58 @@ class LexiconAddValidatorResource(Resource):
 ################################################################################
 
 
-def transform_grew_verif(ligne1, ligne2):  # Voir différences entre deux lignes
-    liste = []
-    if len(ligne1) > len(ligne2):
-        maximum = len(ligne1)
-    else:
-        maximum = len(ligne2)
-    for i in range(maximum):
-        try:
-            if ligne1[i] != ligne2[i]:
-                liste.append(i)
-        except IndexError:
-            liste.append(i)
-    return liste
+def transform_grew_verif(ligne1, ligne2): #Voir différences entre deux lignes
+	liste=[]
+	if len(ligne1) > len(ligne2): 
+		maximum = len(ligne1)
+	else: 
+		maximum = len(ligne2)
+	for i in range(maximum):
+		try:
+			if ligne1[i] != ligne2[i]:
+				liste.append(i)
+		except IndexError:
+			liste.append(i)
+	#print("transform_grew_verif",liste)
+	return liste
 
-
-def transform_grew_get_pattern(ligne, dic, comp):
-    pattern = "X" + str(comp) + '[form="' + ligne[0] + '"'
-    for element in range(1, len(ligne)):
-        if element == len(ligne) - 1:
-            # print(element, ligne[element], dic[element])
-            if ligne[element] != "_" and "=" in ligne[element]:  # features
-                # upos="DET", Number=Sing, PronType=Dem, lemma="dat"
-                mot = ligne[element].split("|")
-                pattern = pattern + ", " + ", ".join(mot)
-        else:
-            pattern = pattern + ", " + dic[element] + '="' + ligne[element] + '"'
-    pattern = pattern + "]"
-    return pattern
+def transform_grew_get_pattern(ligne, dic, comp): 
+	pattern = "X" + str(comp) + '[form=\"' + ligne[0] + '\"'
+	for element in range(1,len(ligne)):
+		if element == len(ligne)-1:
+			# print(element, ligne[element], dic[element])
+			if ligne[element] != "_" and '=' in ligne[element]: #features
+				mot = ligne[element].split("|") #Number=Sing, PronType=Dem
+				pattern = pattern + ", " + ", ".join(mot)
+		elif element == 2: # upos = PRON
+			pattern = pattern + ", " + dic[element] + "=" + ligne[element]
+		else:
+			pattern = pattern + ", " + dic[element] + '=\"' + ligne[element] + '\"' # forme=\"dat\", lemma=\"dat\"
+	pattern = pattern + "]"
+	return pattern
 
 
 def transform_grew_get_without(l, l2, comp):
-    les_traits = ""
-    mot = l.split("|")
-    mot2 = l2.split("|")
-    liste_traits = []
-    # for i in mot :
-    # 	if i not in mot2 and i !="_": # suppression de traits 1 non existant dans traits2
-    # 		les_traits = les_traits+"del_feat X"+str(comp)+"."+i.split("=")[0]+';'
-    for i in mot2:
-        if i not in mot and i != "_":  # ajout traits2 non existant dans traits1
-            les_traits = les_traits + "X" + str(comp) + "." + i + "; "
-            liste_traits.append(i)
-    # print(les_traits, liste_traits)
-    # print (without, liste_traits, len(liste_traits))
-    if len(liste_traits) == 0:
-        liste_traits = False
-    return les_traits, liste_traits
-
+	mot = l.split("|")
+	mot2 = l2.split("|")
+	les_traits = []
+	liste_traits = []
+	feats_str = ""
+	# for i in mot :
+	# 	if i not in mot2 and i !="_": # suppression de traits 1 non existant dans traits2
+	# 		les_traits = les_traits+"del_feat X"+str(comp)+"."+i.split("=")[0]+';'
+	for i in mot2:
+		if i not in mot and i != "_": # ajout traits2 non existant dans traits1
+			liste_traits.append(i)
+	# print(les_traits, liste_traits)
+	# print (without, liste_traits, len(liste_traits))
+	if len(liste_traits) == 0:
+		feats_str = False
+	if liste_traits:
+		les_traits.append("X" + str(comp) + "[" + ",".join(liste_traits) + "]")
+		for feat in liste_traits:
+			feats_str += "X" + str(comp) + "." + feat + "; "
+	return les_traits, feats_str
 
 # def transform_grew_get_features(l2, comp):
 # 	les_traits=""
@@ -274,47 +291,45 @@ def transform_grew_get_without(l, l2, comp):
 # 		without = False
 # 	return les_traits, without
 
-
-def transform_grew_traits_corriges(l, comp):  # lignes2[4]=="_"
-    traits = ""
-    mot = l.split("|")
-    for i in mot:  # suppression des traits 1
-        traits = traits + "del_feat X" + str(comp) + "." + i.split("=")[0] + "; "
-    return traits
-
+def transform_grew_traits_corriges(l, l2, comp): # différence entre deux feats
+	traits = ''
+	mot1 = l.split("|")
+	print(mot1, l,l2)
+	if l2 == "_":
+		for i in mot1:
+			traits = traits + "del_feat X" + str(comp) + "." + i.split("=")[0] + '; '
+	else:
+		mot2 = l2.split("|")
+		print(mot2)
+		for i in mot1:  # suppression des traits 1
+			if i not in mot2:
+				traits = traits + "del_feat X" + str(comp) + "." + i.split("=")[0] + '; '
+	return traits
 
 def transform_grew_get_commands(resultat, ligne1, ligne2, dic, comp):
-    correction = ""
-    commands = ""
-    without_traits = ""
-    list_traits2 = ""
-    for e in resultat:
-        if e == 4:  # si traits sont différents
-            # try :
-            if ligne2[e] != "_":
-                if ligne2[e] != "":  # insertion des traits
-                    list_traits2, without_traits = transform_grew_get_without(
-                        ligne1[e], ligne2[e], comp
-                    )
-                    commands = commands + list_traits2
-                    # print(without_traits) OK
-            else:  # si on doit supprimer les traits de ligne1
-                traits_a_supprimer = transform_grew_traits_corriges(ligne1[e], comp)
-                commands = commands + traits_a_supprimer
-            # except IndexError:
-            # 	if len(ligne2) < 5:
-            # 		traits_a_supprimer = transform_grew_traits_corriges(ligne1[e], comp)
-            # 		commands=commands+traits_a_supprimer
-            # 	elif len(ligne1) < 5:
-            # 		list_traits2, without_traits = transform_grew_get_features(ligne2[e], comp)
-            # 		commands=commands+list_traits2
-            # 	pass
-        else:  # si la différence n'est pas trait
-            commands = (
-                commands + "X" + str(comp) + "." + dic[e] + '="' + ligne2[e] + '"; '
-            )
-    if without_traits == False:
-        correction = correction + commands
-    else:
-        correction = correction + commands
-    return correction, list_traits2
+	correction = ""
+	commands = ""
+	# without_traits = ""
+	list_traits2 = []
+	temp_var = ""
+	for e in resultat:
+		if e == 4: #si traits sont différents
+			# try :
+			#print(len(ligne1[e].split("|")), len(ligne2[e].split("|")))
+			if ligne2[e] != "_" and len(ligne1[e].split("|")) < len(ligne2[e].split("|")) or ligne1[e] == "_":
+				if ligne2[e] != "": #insertion des traits
+					list_traits2, feats_str = transform_grew_get_without(ligne1[e], ligne2[e], comp)
+					temp_var = ",".join(list_traits2)
+					#print(temp_var,"123123", list_traits2)
+					commands = commands + feats_str
+					#print(without_traits,"1112222333", list_traits2)
+			else: #si on doit supprimer les traits de ligne1 :
+				#print("transform_grew_get_commands vers traits_a_supprimer", ligne1[e],ligne2[e], comp)
+				traits_a_supprimer = transform_grew_traits_corriges(ligne1[e], ligne2[e], comp)
+				commands = commands + traits_a_supprimer
+		else: # si la différence n'est pas trait
+			#print(e, dic, comp, ligne1, ligne2)
+			commands = commands + "X" + str(comp) + "." + dic[e] + '=\"' + ligne2[e] + '\"; '
+	correction = correction + commands
+	#print(correction, "------", commands)
+	return correction, temp_var
